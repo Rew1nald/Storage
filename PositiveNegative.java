5c93db2a6af7a1b61724d22ae4d6ca236c56bb09
Rew1nald <francinjaspeo.0205@gmail.com>
2023-03-25 12:55:43 +0800

    Test

---
+++ PositiveNegative.java
@@ -0,0 +1,31 @@
+// Write a Java program to get a number from the user and
+// print whether it is positive or negative.
+
+import java.util.*;
+
+public class PositiveNegative
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Input number: ");
+        int num = input.nextInt();
+
+        if(num > 0)
+        {
+            System.out.println("Number is positive");
+        }
+        else if(num < 0)
+        {
+            System.out.println("Number is negative");
+        }
+        else
+        {
+            System.out.println("Number is zero");
+        }
+        
+        input.close();
+        System.exit(0);
+    }
+}
