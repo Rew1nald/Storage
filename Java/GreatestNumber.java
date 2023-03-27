fa988c1c3f62abfec7cf68c82b7a98ab62c54cb7
Rew1nald <francinjaspeo.0205@gmail.com>
2023-03-27 16:55:56 +0800

    Added

---
+++ Java/GreatestNumber.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class GreatestNumber 
+{
+    public static void main(String[] args)
+    {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Input the 1st number: ");
+        int num1 = input.nextInt();
+        System.out.print("Input the 2nd number: ");
+        int num2 = input.nextInt();
+        System.out.print("Input the 3rd number: ");
+        int num3 = input.nextInt();
+
+        if(num1 > num2)
+        {
+            if(num1 > num3)
+            {
+                System.out.println("The greatest: " +num1);
+            }
+            else
+            {
+                System.out.println("The greatest: " +num3);
+            }
+        }
+
+        if(num2 > num1)
+        {
+            if(num2 > num3)
+            {
+                System.out.println("The greatest: " +num2);
+            }
+            else
+            {
+                System.out.println("The greatest: " +num3);
+            }
+        }
+
+        input.close();
+        System.exit(0);
+    }
+}
