e820f10888b9405a6590f270815a9884b5f046c4
Rew1nald <francinjaspeo.0205@gmail.com>
2023-05-07 12:20:57 +0800

    Nothing really

---
+++ Java/Project/QuizBee.java
@@ -0,0 +1,13 @@
+package Project;
+import java.util.*;
+
+public class QuizBee{
+    public static Scanner input = new Scanner(System.in);
+    public static void main(String[] args){
+        System.out.println("Welcome to the Quiz Bee!");
+            System.out.println("What would you like to do?");
+            System.out.println("A = Answer the ");
+            System.out.println("X = Exit the program");
+        System.exit(0);
+    }
+}
